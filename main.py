5a3bd1a4338089e1bdde2592e119319858edd27c
Maher AMARA <maheramara32@yahoo.fr>
2022-06-03 22:42:36 +0100

    main script

---
+++ main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+__author__ = "Maher Amara"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
